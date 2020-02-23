4a544543f597ce7b5214c389fdec3433f03b0fe0
Dzhamal <dabdulbasirov@sdvor.com>
2020-02-23 13:28:16 +0300

    Fixed bugs

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -11,6 +11,8 @@ export default class Calculator extends VueComponent {
   isProccessing: boolean = false
 
   private calculate(): void {
+    if (!this.formula) return
+
     this.isProccessing = true
     setTimeout(() => {
       this.result = String(eval(this.formula))
@@ -26,12 +28,14 @@ export default class Calculator extends VueComponent {
 
   private inputToFormula(e: Event): void {
     if (this.isProccessing) return
-
+    
     const target: any = e.target
     const formulaInputValue: string = target.dataset.value
-
+    
     const signs: string[] = ['+', '-']
 
+    if (!this.formula && signs.includes(formulaInputValue)) return
+    
     if (signs.includes(this.formula[this.formula.length - 2]) && signs.includes(formulaInputValue)) return
 
     if (signs.includes(formulaInputValue)) this.formula += ` ${formulaInputValue} `
@@ -40,26 +44,26 @@ export default class Calculator extends VueComponent {
 
   public render(): JSX.Element {
     return (
-      <div class={styles.calculator}>
-        <div class={styles.display}>{
+      <div class={styles.calculator} tabindex="0">
+        <div class={styles.display} tabindex="0">{
           this.formula.split(' ')
           .map(str => (<span class={styles.formula}> {str}</span>))
         }</div>
-        <div class={styles.display}>{this.result && `= ${this.result}`}</div>
-        <div class={styles.btn} role="button" data-value="7" onClick={this.inputToFormula}>7</div>
-        <div class={styles.btn} role="button" data-value="8" onClick={this.inputToFormula}>8</div>
-        <div class={styles.btn} role="button" data-value="9" onClick={this.inputToFormula}>9</div>
-        <div class={styles.btnOperator} role="button" onClick={this.clean}>C</div>
-        <div class={styles.btn} role="button" data-value="4" onClick={this.inputToFormula}>4</div>
-        <div class={styles.btn} role="button" data-value="5" onClick={this.inputToFormula}>5</div>
-        <div class={styles.btn} role="button" data-value="6" onClick={this.inputToFormula}>6</div>
-        <div class={styles.btnOperator} role="button" data-value="-" onClick={this.inputToFormula}>-</div>
-        <div class={styles.btn} role="button" data-value="1" onClick={this.inputToFormula}>1</div>
-        <div class={styles.btn} role="button" data-value="2" onClick={this.inputToFormula}>2</div>
-        <div class={styles.btn} role="button" data-value="3" onClick={this.inputToFormula}>3</div>
-        <div class={styles.btnOperator} role="button" data-value="+" onClick={this.inputToFormula}>+</div>
-        <div class={styles.btnZero} role="button" data-value="0" onClick={this.inputToFormula}>0</div>
-        <div class={styles.btnOperator} role="button" onClick={this.calculate}>=</div>
+        <div class={styles.display} tabindex="0">{this.result && `= ${this.result}`}</div>
+        <button class={styles.btn} tabindex="0" data-value="7" onClick={this.inputToFormula}>7</button>
+        <button class={styles.btn} tabindex="0" data-value="8" onClick={this.inputToFormula}>8</button>
+        <button class={styles.btn} tabindex="0" data-value="9" onClick={this.inputToFormula}>9</button>
+        <button class={styles.btnOperator} tabindex="0" onClick={this.clean}>C</button>
+        <button class={styles.btn} tabindex="0" data-value="4" onClick={this.inputToFormula}>4</button>
+        <button class={styles.btn} tabindex="0" data-value="5" onClick={this.inputToFormula}>5</button>
+        <button class={styles.btn} tabindex="0" data-value="6" onClick={this.inputToFormula}>6</button>
+        <button class={styles.btnOperator} tabindex="0" data-value="-" onClick={this.inputToFormula}>-</button>
+        <button class={styles.btn} tabindex="0" data-value="1" onClick={this.inputToFormula}>1</button>
+        <button class={styles.btn} tabindex="0" data-value="2" onClick={this.inputToFormula}>2</button>
+        <button class={styles.btn} tabindex="0" data-value="3" onClick={this.inputToFormula}>3</button>
+        <button class={styles.btnOperator} tabindex="0" data-value="+" onClick={this.inputToFormula}>+</button>
+        <button class={styles.btnZero} tabindex="0" data-value="0" onClick={this.inputToFormula}>0</button>
+        <button class={styles.btnOperator} tabindex="0" onClick={this.calculate}>=</button>
       </div>
     )
   }
